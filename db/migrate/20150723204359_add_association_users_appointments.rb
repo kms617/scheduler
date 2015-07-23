10c0b7f52c013e1575edb655bc6e4b440c947a3d
Kristin Smith <krysia.codes@gmail.com>
2015-07-23 16:46:23 -0400

    Adds association between Appointments and Users

---
+++ db/migrate/20150723204359_add_association_users_appointments.rb
@@ -0,0 +1,5 @@
+class AddAssociationUsersAppointments < ActiveRecord::Migration
+  def change
+    add_column :appointments, :user_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150723034600) do
+ActiveRecord::Schema.define(version: 20150723204359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20150723034600) do
     t.string   "location"
     t.boolean  "booked",         default: false, null: false
     t.integer  "recruiter_id"
+    t.integer  "user_id"
   end
 
   create_table "delayed_jobs", force: :cascade do |t|
