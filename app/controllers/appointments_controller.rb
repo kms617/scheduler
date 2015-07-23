c06919711e7fc12515f9544b68adb6cd625578f9
Kristin Smith <krysia.codes@gmail.com>
2015-07-23 00:49:45 -0400

    WIP

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -5,6 +5,21 @@ class AppointmentsController < ApplicationController
     @appointments = Appointment.all
   end
 
+  def new
+    @appointment = Appointment.new
+  end
+
+  def create
+    @appointment = Appointment.new(appointment_params)
+    @appointment.recruiter_id = current_user
+
+    if @appointment.save
+      redirect_to appointments_path
+    else
+      render :new
+    end
+  end
+
   def edit
     @appointment = Appointment.find_by(params[:id])
   end
@@ -20,6 +35,6 @@ class AppointmentsController < ApplicationController
   end
 
   def appointment_params
-    params.require(:appointment).permit(:user_id)
+    params.require(:appointment).permit(:scheduled_time, :location, :user_id)
   end
 end
