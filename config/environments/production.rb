5ad462a1ba2b621f84c59f16c870265048620723
Kristin Smith <krysia.codes@gmail.com>
2015-07-27 16:07:18 -0400

    Adds styling to login and signup pages

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-require Rails.root.join("config/smtp")
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -84,20 +84,20 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: ENV.fetch("HOST") }
+  #config.action_mailer.default_url_options = { host: ENV.fetch("HOST") }
 
-  config.action_mailer.default_options = {
+  #config.action_mailer.default_options = {
    from: "no-reply@#{ENV["HOST"]}"
  }
 
- config.action_mailer.delivery_method = :smtp
- config.action_mailer.smtp_settings = {
-   address: ENV["SMTP_HOST"],
-   port: ENV["SMTP_PORT"],
-   user_name: ENV["SMTP_USERNAME"],
-   password: ENV["SMTP_PASSWORD"],
-   domain: ENV["SMTP_DOMAIN"],
-   authentication: :plain
- }
+ #config.action_mailer.delivery_method = :smtp
+ #config.action_mailer.smtp_settings = {
+  # address: ENV["SMTP_HOST"],
+   #port: ENV["SMTP_PORT"],
+  # user_name: ENV["SMTP_USERNAME"],
+#   password: ENV["SMTP_PASSWORD"],
+  # domain: ENV["SMTP_DOMAIN"],
+#   authentication: :plain
+ #}
 end
 Rack::Timeout.timeout = (ENV["RACK_TIMEOUT"] || 10).to_i
