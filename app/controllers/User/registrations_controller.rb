d4a8c066cea1c22596a4216555b31539d90ffa13
Kristin Smith <krysia.codes@gmail.com>
2015-07-27 21:01:39 -0400

    Skips authenticity token for registrations

--- app/controllers/User/registrations_controller.rb
+++ app/controllers/User/registrations_controller.rb
@@ -1,5 +1,6 @@
 class User::RegistrationsController < Devise::RegistrationsController
-
+  skip_before_filter :verify_authenticity_token
+  
   def book_appointment
     repond_to do |format|
       format.js
