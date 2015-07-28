4956ac199b22f0b3628b824d06074152fa93f960
Kristin Smith <krysia.codes@gmail.com>
2015-07-27 21:05:01 -0400

    Removes filter to skip authenticity token

--- app/controllers/User/registrations_controller.rb
+++ app/controllers/User/registrations_controller.rb
@@ -1,6 +1,5 @@
 class User::RegistrationsController < Devise::RegistrationsController
-  skip_before_filter :verify_authenticity_token
-  
+
   def book_appointment
     repond_to do |format|
       format.js
